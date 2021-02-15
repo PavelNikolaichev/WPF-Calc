7e20be6a6c04e4c80914c791cbfae0c39980871c
Pavel Nikolaichev <pavelnikolaichev@gmail.com>
2021-02-15 19:15:04 +0300

    Implemented new features
    Working buttons and operands, also Clear button is working too.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int num1 = 0, num2 = 0;
+        private string operand = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddNum(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int input = int.Parse(button.Content.ToString());
+            if (operand == "")
+            {
+                num1 = num1 * 10 + input;
+                inputOutputField.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = num2 * 10 + input;
+                inputOutputField.Text = num2.ToString();
+            }
+        }
+
+        private void ChangeOperation(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string input = button.Content.ToString();
+            operand = input;
+        }
+
+        private void Equal(object sender, RoutedEventArgs e)
+        {
+            int result = 0;
+            switch (operand)
+            {
+                //TODO: Make percent great again
+                case "%": result = num1 + num2; break;
+                case "/": result = num1 / num2; break;
+                case "*": result = num1 * num2; break;
+                case "-": result = num1 - num2; break;
+                case "+": result = num1 + num2; break;
+            }
+            num1 = result;
+            operand = "";
+            num2 = 0;
+            inputOutputField.Text = num1.ToString();
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            num1 = 0;
+            num2 = 0;
+            operand = "";
+            inputOutputField.Text = num1.ToString();
         }
     }
 }
