683d97a43aabe7d713240bd47396d07cc3ed1ed3
PavelNikolaichev <pavelnikolaichev@gmail.com>
2021-05-11 07:50:57 +0300

    Finally, completed calculator.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -14,13 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Calculator
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
-        private int num1 = 0, num2 = 0;
+        private double num1 = 0, num2 = 0;
         private string operand = "";
         public MainWindow()
         {
@@ -30,33 +27,35 @@ namespace Calculator
         private void AddNum(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            int input = int.Parse(button.Content.ToString());
+            string num = button.Content.ToString();
+            if (inputOutputField.Text == "0")
+                inputOutputField.Text = num;
+            else
+                inputOutputField.Text += num;
+
             if (operand == "")
             {
-                num1 = num1 * 10 + input;
-                inputOutputField.Text = num1.ToString();
+                num1 = double.Parse(inputOutputField.Text);
             }
             else
             {
-                num2 = num2 * 10 + input;
-                inputOutputField.Text = num2.ToString();
+                num2 = double.Parse(inputOutputField.Text);
             }
         }
 
         private void ChangeOperation(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            string input = button.Content.ToString();
-            operand = input;
+            operand = button.Content.ToString();
+            inputOutputField.Text = "0";
         }
 
         private void Equal(object sender, RoutedEventArgs e)
         {
-            int result = 0;
+            double result = 0;
             switch (operand)
             {
-                //TODO: Make percent great again
-                case "%": result = num1 + num2; break;
+                case "%": result = num1 * (num2 / 100); break;
                 case "/": result = num1 / num2; break;
                 case "*": result = num1 * num2; break;
                 case "-": result = num1 - num2; break;
@@ -66,6 +65,20 @@ namespace Calculator
             operand = "";
             num2 = 0;
             inputOutputField.Text = num1.ToString();
+        }
+
+        private void ConvertToDouble(object sender, RoutedEventArgs e)
+        {
+            if (operand == "")
+                SetComma(num1);
+            else
+                SetComma(num2);
+        }
+
+        private void SetComma(double num)
+        {
+            if (!inputOutputField.Text.Contains('.'))
+                inputOutputField.Text += '.';
         }
 
         private void Clear(object sender, RoutedEventArgs e)
